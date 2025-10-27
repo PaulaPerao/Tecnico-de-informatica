cb3387674940fc894f71d6add8509d2a85ea17c4
Paula-Perao <paula.cbgperao@senacsp.edu.br>
2025-10-27 16:34:50 -0300

    atualização

--- 2- Javascript/dom/multas/script.js
+++ 2- Javascript/dom/multas/script.js
@@ -13,8 +13,8 @@ function calcular() {
     if (velocidade <= 40) {
         resp.innerText = 'Você está dentro da velocidade Permitida!'
     } else if (velocidade > 40 && velocidade < 100) {
-        resp.innerHTML = 'Você está acima da velocidade, será <strong MULTADO!<strong>'
+        resp.innerHTML = 'Você está acima da velocidade, será <strong> MULTADO!</strong>'
     } else {
-        resp.innerHTML = 'Você está muito acima da velocidade permitida, será <strong>MULTADO!<strong> e o veículo será<strong>APREENDIDO!<strong>' 
-}
+        resp.innerHTML = 'Você está muito acima da velocidade permitida, será <strong>MULTADO!</strong> e o veículo será <strong> APREENDIDO!</strong> '
+    }
 }
